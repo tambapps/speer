717fcc9ffb1fda2aa3d9f25e75bed1f330f5513a
Tambapps <tambapps@gmail.com>
2021-05-05 06:30:34 +0200

    added tests for strategy

--- src/main/java/com/tambapps/p2p/fandem/sniff/strategy/LastOctetSniffingStrategy.java
+++ src/main/java/com/tambapps/p2p/fandem/sniff/strategy/LastOctetSniffingStrategy.java
@@ -1,30 +1,46 @@
 package com.tambapps.p2p.fandem.sniff.strategy;
 
 import com.tambapps.p2p.fandem.Peer;
+import lombok.SneakyThrows;
+
+import java.net.InetAddress;
 
 public class LastOctetSniffingStrategy implements SniffingStrategy {
 
   // TODO complete me
+  private final byte[] address;
   private final int port;
-  // TODO allow to specify the starting point
-  private int i = 0;
+  private final byte start;
 
-  public LastOctetSniffingStrategy(int port) {
+  // TODO allow to specify the starting point
+  private int i;
+
+  public LastOctetSniffingStrategy(InetAddress address, int port) {
+    this(address, port, (byte) 0);
+  }
+  public LastOctetSniffingStrategy(InetAddress address, int port, byte start) {
+    this.address = address.getAddress();
     this.port = port;
+    this.start = start;
+    reset();
   }
 
   @Override
   public void reset() {
     i = 0;
+    this.address[3] = start;
   }
 
   @Override
   public boolean hasNext() {
-    return i < 255;
+    return i <= 255;
   }
 
+  @SneakyThrows
   @Override
   public Peer next() {
-    return null;
+    Peer peer = Peer.of(InetAddress.getByAddress(address), port);
+    address[3] = (byte) (start + ++i);
+    return peer;
   }
 }
