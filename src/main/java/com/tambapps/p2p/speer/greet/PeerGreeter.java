31e3e7f79b0e59973686551e0b78eb7e5d80086e
Tambapps <tambapps@gmail.com>
2021-05-08 19:32:05 +0200

    code cleaning

--- src/main/java/com/tambapps/p2p/speer/greet/PeerGreeter.java
+++ src/main/java/com/tambapps/p2p/speer/greet/PeerGreeter.java
@@ -11,7 +11,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
