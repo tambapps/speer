4f050f40827d54feeee25a72c5f8a7eb2416fe71
Tambapps <tambapps@gmail.com>
2021-05-08 19:32:53 +0200

    fully implemented SeekedPeerSupplier

--- src/main/java/com/tambapps/p2p/speer/seek/SeekedPeerSupplier.java
+++ src/main/java/com/tambapps/p2p/speer/seek/SeekedPeerSupplier.java
@@ -1,20 +1,67 @@
 package com.tambapps.p2p.speer.seek;
 
 import com.tambapps.p2p.speer.Peer;
+import com.tambapps.p2p.speer.seek.strategy.SeekingStrategy;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingDeque;
 
-@AllArgsConstructor
-public class SeekedPeerSupplier {
+/**
+ * Supplier of seeked peer. The seeking process will be asynchronous if an executor service is
+ * provided
+ */
+public class SeekedPeerSupplier implements PeerSeeker.SeekListener {
 
-  private BlockingQueue<Peer> peersQueue = new LinkedBlockingDeque<>();
+  private final BlockingQueue<Peer> peersQueue = new LinkedBlockingDeque<>();
+  private final SeekingStrategy seekingStrategy;
   private final PeerSeeker seeker;
   private final ExecutorService executorService;
 
+  public SeekedPeerSupplier(SeekingStrategy seekingStrategy,
+      PeerSeeking seeking) {
+    this(null, seekingStrategy, seeking);
+  }
+
+  public SeekedPeerSupplier(ExecutorService executorService,
+      SeekingStrategy seekingStrategy,
+      PeerSeeking seeking) {
+    this.executorService = executorService;
+    this.seekingStrategy = seekingStrategy;
+    this.seeker = new PeerSeeker(seeking, this);
+  }
+
   public Peer get() throws InterruptedException {
+    if (peersQueue.isEmpty()) {
+      if (executorService != null) {
+        asyncSeek();
+      } else {
+        seeker.seek(seekingStrategy);
+      }
+    }
     return peersQueue.take();
+  }
+
+  private void asyncSeek() throws InterruptedException {
+    List<Future<List<Peer>>> seek = seeker.seek(seekingStrategy, executorService);
+    for (Future<List<Peer>> future : seek) {
+      if (!peersQueue.isEmpty()) {
+        return;
+      }
+      try {
+        future.get();
+      } catch (ExecutionException e) {
+        throw new RuntimeException("Error while executing seeking", e);
+      }
+    }
+  }
+
+  @Override
+  public void onPeersFound(List<Peer> peers) {
+    peersQueue.addAll(peers);
   }
 }
--- src/test/java/com/tambapps/p2p/speer/PeerSeekGreetTest.java
+++ src/test/java/com/tambapps/p2p/speer/PeerSeekGreetTest.java
@@ -6,12 +6,16 @@ import com.tambapps.p2p.speer.greet.PeerGreeter;
 import com.tambapps.p2p.speer.greet.PeerGreetings;
 import com.tambapps.p2p.speer.seek.PeerSeeker;
 import com.tambapps.p2p.speer.seek.PeerSeeking;
+import com.tambapps.p2p.speer.seek.SeekedPeerSupplier;
 import com.tambapps.p2p.speer.seek.strategy.LastOctetSeekingStrategy;
+import com.tambapps.p2p.speer.seek.strategy.SeekingStrategy;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.net.InetAddress;
-import java.net.ServerSocket;
+import java.io.IOException;
+import java.net.SocketException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,32 +28,60 @@ public class PeerSeekGreetTest {
   private static final Peer PEER1 = Peer.of("127.0.0.2", 8081);
   private static final Peer PEER2 = Peer.of("127.0.0.2", 8082);
 
-  private final PeerGreetings greetings = (peers, outputStream) -> outputStream.writeUTF(
+  private static final int GREETER_PORT = 8081;
+  private static final PeerGreetings GREETINGS = (peers, outputStream) -> outputStream.writeUTF(
       peers.stream().map(Peer::toString).collect(Collectors.joining("\n"))
   );
-  private final PeerSeeking seeking = inputStream ->
+  private static final SeekingStrategy STRATEGY =
+      new LastOctetSeekingStrategy("127.0.0.0", GREETER_PORT);
+  private static final PeerSeeking SEEKING = inputStream ->
       Arrays.stream(inputStream.readUTF().split("\\n"))
           .map(Peer::parse).collect(Collectors.toList());
 
-  private final ExecutorCompletionService<Void> executor = new ExecutorCompletionService<>(
+  private static final ExecutorCompletionService<Void> executor = new ExecutorCompletionService<>(
       Executors.newFixedThreadPool(2)
   );
 
-  @Test
-  public void test() throws Exception {
-    PeerGreeter greeter = new PeerGreeter(Arrays.asList(PEER1, PEER2), greetings);
-    Peer peer = Peer.of("127.0.0.1", 8081);
+  private static final PeerGreeter GREETER = new PeerGreeter(Arrays.asList(PEER1, PEER2), GREETINGS);
+  private static ServerPeer serverPeer;
+
+  @BeforeAll
+  public static void initGreeter() throws Exception {
+    Peer peer = Peer.of("127.0.0.1", GREETER_PORT);
+    serverPeer = new ServerPeer(peer);
     executor.submit(() -> {
-      try (ServerSocket serverSocket = new ServerSocket(peer.getPort(), 10, peer.getIp())) {
-        greeter.greetOne(serverSocket);
+      try {
+        LOGGER.info("Starting greeting at {}", peer);
+        GREETER.greet(serverPeer);
+      } catch (SocketException e) {
+        LOGGER.info("Server socket was closed, stopped greeting.", e);
       }
       return null;
     });
-    PeerSeeker seeker = new PeerSeeker(seeking, (peers) -> LOGGER.info("Found peers {}", peers));
+    Thread.sleep(1000L);
+  }
 
-    Set<Peer> peers = seeker
-        .seek(new LastOctetSeekingStrategy(InetAddress.getByName("127.0.0.0"), peer.getPort()));
+  @AfterAll
+  public static void disposeGreeter() throws IOException {
+    serverPeer.close();
+  }
+
+  @Test
+  public void test() {
+    PeerSeeker seeker = new PeerSeeker(SEEKING, (peers) -> LOGGER.info("Found peers {}", peers));
+
+    Set<Peer> peers = seeker.seek(STRATEGY);
 
     assertEquals(new HashSet<>(Arrays.asList(PEER1, PEER2)), peers);
+  }
+
+  @Test
+  public void testSupplier() throws Exception {
+    SeekedPeerSupplier supplier = new SeekedPeerSupplier(STRATEGY, SEEKING);
+
+    Peer peer1 = supplier.get();
+    Peer peer2 = supplier.get();
+
+    assertEquals(Arrays.asList(PEER1, PEER2), Arrays.asList(peer1, peer2));
   }
 }
