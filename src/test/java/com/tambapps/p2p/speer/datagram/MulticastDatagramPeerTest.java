4fdee5ee58444594a147a28c6964636e73dd5809
Tambapps <tambapps@gmail.com>
2021-07-14 16:45:19 +0200

    fixed multicast test

--- src/test/java/com/tambapps/p2p/speer/datagram/MulticastDatagramPeerTest.java
+++ src/test/java/com/tambapps/p2p/speer/datagram/MulticastDatagramPeerTest.java
@@ -5,10 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.tambapps.p2p.speer.Peer;
 import com.tambapps.p2p.speer.util.DangerousConsumer;
 import com.tambapps.p2p.speer.util.PeerUtils;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -22,14 +21,7 @@ public class MulticastDatagramPeerTest {
   private static final InetAddress ADDRESS = PeerUtils.getAddress("127.0.0.2");
   private static final InetAddress MULTICAST_ADDRESS = PeerUtils.getAddress("230.0.0.0");
   private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
-  private static MulticastDatagramPeer datagramPeer;
   private Future<?> future;
-
-  @BeforeAll
-  public static void initCommunicator() throws IOException {
-    datagramPeer = new MulticastDatagramPeer();
-    datagramPeer.getSocket().setBroadcast(true);
-  }
 
   @AfterEach
   public void dispose() {
@@ -37,35 +29,38 @@ public class MulticastDatagramPeerTest {
       future.cancel(true);
     }
   }
-  private void runInBackground(DangerousConsumer<MulticastDatagramPeer> consumer) {
+
+  @SneakyThrows
+  private MulticastDatagramPeer runInBackground(DangerousConsumer<MulticastDatagramPeer> consumer) {
+    MulticastDatagramPeer datagramPeer = new MulticastDatagramPeer();
     this.future = EXECUTOR.submit(() -> {
       while (!Thread.interrupted()) {
         try {
           consumer.accept(datagramPeer);
           Thread.sleep(1000L);
-        } catch (IOException e) {
-          e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
           break;
         }
       }
     });
+    return datagramPeer;
   }
+
   @Test
   public void testCommunication() throws IOException {
-    runInBackground(communicator -> communicator.send("Hello World", ADDRESS, 5000));
-    try (MulticastDatagramPeer client = new MulticastDatagramPeer(Peer.of(ADDRESS, 5000))) {
-      assertEquals("Hello World", client.receiveString(1024));
+    try (MulticastDatagramPeer datagramPeer = runInBackground(communicator -> communicator.send("Hello World", ADDRESS, 5000));
+        MulticastDatagramPeer client = new MulticastDatagramPeer(Peer.of(ADDRESS, 5000))) {
+      assertEquals("Hello World", client.receiveString());
     }
   }
 
-  @Disabled("Can't be tested with only one device (?)")
   @Test
   public void testMulticast() throws IOException {
-    runInBackground(communicator -> communicator.send("Hello World", MULTICAST_ADDRESS, 5000));
-    MulticastDatagramPeer client = new MulticastDatagramPeer(5000);
-    client.joinGroup(MULTICAST_ADDRESS);
-    assertEquals("Hello World", client.receiveString(1024));
+    try (MulticastDatagramPeer datagramPeer = runInBackground(communicator -> communicator.send("Hello World", MULTICAST_ADDRESS, 5000));
+        MulticastDatagramPeer client = new MulticastDatagramPeer(5000)) {
+      client.joinGroup(MULTICAST_ADDRESS);
+      assertEquals("Hello World", client.receiveString());
+    }
   }
 
   @AfterAll
