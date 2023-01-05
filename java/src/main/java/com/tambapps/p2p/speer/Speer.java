63f5959357a052679bb880c0e1dc6144e48df0ad
Nelson Fonkoua <nfonkoua@weborama.com>
2023-01-05 18:24:44 +0100

    bumped version

--- java/src/main/java/com/tambapps/p2p/speer/Speer.java
+++ java/src/main/java/com/tambapps/p2p/speer/Speer.java
@@ -2,7 +2,7 @@ package com.tambapps.p2p.speer;
 
 public final class Speer {
 
-  public static final String VERSION = "0.0.1";
+  public static final String VERSION = "1.4-SNAPSHOT";
 
   private Speer() {
   }
