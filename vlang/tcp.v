f8ece75fd61bb088ae0e2d884f6584ab3006e44b
Nelson Fonkoua <tambue@hotmail.fr>
2023-01-06 06:41:42 +0100

    started adding tcp stuff

--- vlang/tcp.v
+++ vlang/tcp.v
@@ -2,28 +2,59 @@ module main
 
 import net
 
-fn new_peer_server(addr string) !PeerServer {
-	if mut listener := net.listen_tcp(addr) {
-		return PeerServer {
-			listener: listener
-		}
-	} else {
-		return err
+struct EndpointParams {
+
+}
+pub fn new_peer_server_ipv6(endpoint string) !&PeerServer {
+	return new_peer_server(endpoint, net.AddrFamily.ip6)
+}
+
+pub fn new_peer_server_ipv4(endpoint string) !&PeerServer {
+	return new_peer_server(endpoint, net.AddrFamily.ip)
+}
+
+pub fn new_peer_server(endpoint string, addrFamily net.AddrFamily) !&PeerServer {
+	address,port := net.split_address(endpoint)!
+	mut listener := net.listen_tcp(addrFamily, endpoint)!
+
+	return &PeerServer {
+		address: address
+		port: port
+		listener: listener
 	}
 }
+
+pub fn new_peer_connection(endpoint string, addrFamily net.AddrFamily) !&PeerConnection {
+	//net.dial_tcp()
+}
+
 struct PeerServer {
+pub:
+	address string
+	port int
 mut:
 	listener &net.TcpListener
 
 }
 
-fn (self &PeerServer) accept() !PeerConnection {
-	return self.listener.
+fn (mut self PeerServer) accept() !&PeerConnection {
+	listener := self.listener.accept()!
+	return &PeerConnection{connection: listener}
 }
-fn (self &PeerServer) close() ! {
+fn (mut self PeerServer) close() ! {
 	self.listener.close()!
 }
-struct PeerConnection {
 
+struct PeerConnection {
+mut:
+	connection net.TcpConn
 }
 
+fn (self PeerConnection) read_byte() !u8 {
+	buf := []u8{}
+	self.connection.read(buf)!
+}
+
+fn (self PeerConnection) write_byte(b u8) ! {
+	self.connection.write([b])!
+}
--- vlang/tcp_test.v
+++ vlang/tcp_test.v
@@ -1,3 +1,23 @@
 module main
 
+import time
 
+fn test_connection() {
+
+  println("starting socket")
+  mut server := new_peer_server_ipv4("localhost:8081")!
+
+  assert server.address == 'localhost'
+  assert server.port == 8081
+
+  t := spawn fn [mut server] () {
+    connection := server.accept()
+    time.sleep(2 * time.millisecond)
+    println(connection.read_byte()!)
+  }()
+
+
+  //listener.accept()!
+
+  t.wait()
+}
